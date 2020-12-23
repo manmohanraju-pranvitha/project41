12558b84e4c94bb50220ea9a34375199a694b27a
manmohanraju-pranvitha <73023267+manmohanraju-pranvitha@users.noreply.github.com>
2020-12-23 15:47:18 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,53 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score=0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+    getscore(){
+        var playerIndex = "players/player" + this.index+"score";
+        database.ref(playerIndex).on("value",(data)=>{
+          this.score=data.val();
+          console.log(this.score);
+
+        })
+      }
+      static  updatescore(playerscore){
+        var playerIndex = "players/player" + this.index+"score";
+        database.ref(playerIndex).update({
+          score:playerscore
+        })
+      }
+    }
+    
+
